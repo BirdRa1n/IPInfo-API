6acc35657a5db49e2ea4c9a0ca2e55eefcb5f13a
Dário Jr <53487868+BirdRa1n@users.noreply.github.com>
2025-07-18 15:12:32 -0300

    Adicionando rota auth

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,15 @@
 import express, { Request, Response } from 'express';
 import Location from '../controllers/location';
+import Auth from '../controllers/user/auth';
 const router = express.Router();
 
 router.get('/location/:ip', (req: Request, res: Response) => {
     Location({ req, res });
+});
+
+//Auth
+router.post('/auth', (req: Request, res: Response) => {
+    Auth({ req, res });
 });
 
 export default router;
