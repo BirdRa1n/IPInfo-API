8760f64349ec627ca35fa829b08b7e080fd13d7f
Dário Jr <53487868+BirdRa1n@users.noreply.github.com>
2025-07-18 14:36:39 -0300

    Configurando controller location no router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,17 +1,9 @@
 import express, { Request, Response } from 'express';
-import fetchLocationByIp from '../utils/getLocationByIp';
-
+import Location from '../controllers/location';
 const router = express.Router();
 
-// Route to get location information by IP
-router.get('/location/:ip', async (req: Request, res: Response) => {
-    const ipAddress = req.params.ip;
-    try {
-        const locationData = await fetchLocationByIp(ipAddress);
-        res.json(locationData);
-    } catch (error) {
-        res.status(500).send('Error retrieving location information');
-    }
+router.get('/location/:ip', (req: Request, res: Response) => {
+    Location({ req, res });
 });
 
 export default router;
