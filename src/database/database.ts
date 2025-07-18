571c9de5abf353c366b76d759d3b893f9808abc5
Dário Jr <53487868+BirdRa1n@users.noreply.github.com>
2025-07-18 14:45:18 -0300

    desativando logs do sequelize

--- src/database/database.ts
+++ src/database/database.ts
@@ -3,6 +3,7 @@ import { Sequelize, DataTypes } from 'sequelize';
 const sequelize = new Sequelize({
     dialect: 'sqlite',
     storage: 'src/database/database.sqlite',
+    logging: false
 });
 
 export default sequelize;
